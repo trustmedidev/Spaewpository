176b996ee3ca25e84a9eb7fdd6be99df5cec1530
trustmedidev <purojit@trustmedi.com>
2016-12-31 12:45:50 +0530

    commit by soma

---
+++ EntityLayer/BOMEL.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+   public class BOMEL
+    {
+       public int ItemCd { get; set; }
+       public int UnitCd { get; set; }
+       public string Item { get; set; }
+       public string Unit { get; set; }
+
+    }
+}
