90314d20004478da8e97238e07c1c11d91bdc710
trustmedidev <purojit@trustmedi.com>
2017-01-03 19:24:35 +0530

    PB itemopeningstock

---
+++ DataAccessLayer/ItemOpeningstock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Repository;
+using System.Data.Entity;
+using System.Windows.Forms;
+using EntityLayer;
+namespace DataAccessLayer
+{
+    class ItemOpeningstock : SpaPracticeEntities
+    {
+           public int InsertUpdateOpeningstockHdr(tblitemopeningheader objOpeningstockHdr, tblitemopeningdetail obopeningStockDtlj)
+           {
+               return 0;
+           }
+        
+    }
+}
