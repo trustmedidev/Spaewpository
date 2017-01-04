f16b322d1f243526e821b646ef08022b25640e43
trustmedidev <purojit@trustmedi.com>
2017-01-04 19:09:50 +0530

    StockDal

---
+++ DataAccessLayer/StockDAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DataAccessLayer.Repository;
+using System.Data.Entity;
+using System.Windows.Forms;
+using EntityLayer;
+namespace DataAccessLayer
+{
+  public  class StockDAL : SpaPracticeEntities
+    {
+        public void InsertStock()
+        {
+
+        }
+    }
+}
