30040802a5f09d30150380a5573a997071534adf
trustmedidev <purojit@trustmedi.com>
2017-01-12 13:10:33 +0530

    PB Branchrightel

---
+++ EntityLayer/BranchRightEL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer
+{
+  public  class BranchRightEL
+    {
+        public int Code { get; set; }
+        public int UserCD { get; set; }
+        public int BranchCd { get; set; }
+        public string branchName { get; set; }
+        public int RoleCD { get; set; }
+        public bool AllowYN { get; set; }
+    }
+}
